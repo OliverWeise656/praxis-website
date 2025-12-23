047194367fb2c2102dae1b1cbbb135eae8397343
OliverWeise656 <dro_weise@yahoo.de>
2025-12-23 15:10:16 +0100

    Add Footer component to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default function RootLayout({
   children,
@@ -11,6 +12,7 @@ export default function RootLayout({
       <body className="bg-[#F7F8F6] text-gray-800">
         <Header />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
