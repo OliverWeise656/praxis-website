c30aad7450dc2c779d2e636a1b02bdc8e2228f25
OliverWeise656 <dro_weise@yahoo.de>
2025-12-23 15:07:24 +0100

    Update RootLayout to include Header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,6 @@
+import "./globals.css";
+import Header from "@/components/Header";
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="de">
-      <body>{children}</body>
+      <body className="bg-[#F7F8F6] text-gray-800">
+        <Header />
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
